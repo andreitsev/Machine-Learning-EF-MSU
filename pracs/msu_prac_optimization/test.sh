bfcff4d79b03219d256d84e206531d6a6db070e0
Anton Andreytsev <antonandreytsev@MacBook-Pro-Anton.local>
2021-09-28 09:44:21 +0300

    Добавил unzip_blackboxfunction.py и изменил test.sh

--- pracs/msu_prac_optimization/test.sh
+++ pracs/msu_prac_optimization/test.sh
@@ -3,5 +3,9 @@
 echo Тестируем submission.py
 python3 /prac_folder/test_submission.py
 echo
-echo Тестируем blackbox_optimize.py
-python3 /prac_folder/blackbox_run.py
+
+if [[ -f ./blackboxfunction.py ]]
+then
+  echo Тестируем blackbox_optimize.py
+  python3 /prac_folder/blackbox_run.py
+fi
