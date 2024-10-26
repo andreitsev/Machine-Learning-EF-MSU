d0fb7ccb545c4fc0072f2b50ecf54ebe97196b40
joitandr <anton.andreitsev@constructor.io>
2024-10-26 16:27:37 +0300

    checked that code passed current public tests

--- pracs/msu_prac_recommendations/public_tests.py
+++ pracs/msu_prac_recommendations/public_tests.py
@@ -602,10 +602,9 @@ user2user_get_items_scores_test_cases = [
         #      [1, 1, 1, 1, 0, 0, 0], # user3     jaccard_sim(0, 3) = 2/7 = 0.286
         #      [1, 1, 0, 0, 1, 1, 0], # user4     jaccard_sim(0, 4) = 3/6 = 0.5
         #      [1, 0, 0, 0, 0, 1, 0], # user5     jaccard_sim(0, 5) = 1/6 = 0.167
-
         # taking only users, that are closer than alpha (in this case 0.3). So here we consider 
         # user2 and user4 to be similar to user0, so predicted ratings (\hat{r}_{ui}) here would be:
-        
+    
         #   2/5 * [0, 0, 0, 0, 1, 0, 1] 
         # + 
         #   3/6 * [1, 1, 0, 0, 1, 1, 0]
